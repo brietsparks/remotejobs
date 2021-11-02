61d75a45af9f9306443c56be24e3d5f71b2b5046
Briet Sparks <brietsparks@gmail.com>
2021-11-01 22:42:54 -0500

    add theme

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next';
+import { ThemeProvider } from '@material-ui/core';
+
+import { appTheme } from '../styles/theme';
+import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default appWithTranslation(App);
---
+++ web/src/styles/theme.ts
@@ -0,0 +1,107 @@
+import { createTheme, Theme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+export type { Theme };
+
+const borderRadius = 4;
+
+// @ts-ignore
+export const appTheme = createTheme({
+  palette: {
+    primary: { main: '#333' },
+    secondary: red,
+  },
+  typography: {
+    button: {
+      textTransform: 'none'
+    },
+    fontSize: 14,
+    subtitle1: {
+      fontSize: 12,
+      color: 'rgba(0, 0, 0, 0.54)',
+    },
+  },
+  overrides: {
+    MuiOutlinedInput: {
+      root: { borderRadius },
+      multiline: { borderRadius },
+    },
+    MuiIconButton: {
+      root: {
+        borderRadius,
+      },
+    },
+    MuiButton: {
+      root: {
+        borderRadius,
+      },
+    },
+    MuiTooltip: {
+      tooltip: {
+        borderRadius,
+      },
+    },
+    MuiLink: {
+      root: {
+        cursor: 'pointer',
+      },
+    },
+    MuiDialog: {
+      paper: {
+        borderRadius: borderRadius * 2
+      }
+    },
+    MuiChip: {
+      root: {
+        transition: undefined,
+      },
+      deleteIcon: {
+        width: undefined,
+        height: undefined,
+      },
+      clickable: {
+        '&:active': {
+          boxShadow: 'none'
+        }
+      }
+    }
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true // No ripple on the whole application
+    },
+    MuiDialog: {
+      transitionDuration: 0,
+    },
+    MuiPopover: {
+      PaperProps: { square: true },
+    },
+    MuiTooltip: {
+      // enterDelay: 1200,
+      arrow: true,
+    },
+    MuiIconButton: {
+      size: 'small',
+    },
+    MuiButton: {
+      disableRipple: true,
+    },
+    MuiTextField: {
+      variant: 'outlined',
+      // margin: 'dense',
+      InputLabelProps: {
+        shrink: true,
+      },
+    },
+    MuiPaper: {
+      square: true,
+    },
+    MuiFormControl: {
+      variant: 'outlined',
+      // margin: 'dense',
+    },
+    MuiCheckbox: {
+      color: 'primary'
+    }
+  },
+});
